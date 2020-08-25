d08d6643089e1bf3cb51cba9da6098e537c00f06
FatimaWarraich <66510870+FatimaWarraich@users.noreply.github.com>
2020-08-25 15:54:49 +0500

    2nd commit

---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,29 @@
+import React, {useContext} from 'react'
+
+// import the Transaction component
+
+import {Transaction} from './Transaction';
+
+//import the Global State
+import {GlobalContext} from '../context/GlobalState';
+
+export const TransactionHistory = () => {
+
+  const {transactions}= useContext(GlobalContext);
+
+  console.log(transactions);
+
+  return (
+            <div>
+             <h3>
+                Transaction History
+             </h3>
+             <ul className="list">
+                 {transactions.map(transaction => (
+                  <Transaction key = { transaction.id} transaction={transaction}/>
+                  )
+                  )}                     
+             </ul>
+          </div>
+    )
+}
